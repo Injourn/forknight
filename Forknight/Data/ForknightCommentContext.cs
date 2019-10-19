c6a9b27407813eab2501f78950541bfd4402875a
Scott-GH <thatguywhowas@live.com>
2019-10-18 17:07:55 -0700

    Added Comment Model

---
+++ Forknight/Data/ForknightCommentContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Forknight.Models;
+
+namespace Forknight.Data
+{
+    public class ForknightCommentContext : DbContext
+    {
+        public ForknightCommentContext(DbContextOptions<ForknightCommentContext> options) : base(options) {
+
+        }
+        public DbSet<Comment> Comment { get; set; }
+
+    }
+}
---
+++ Forknight/Models/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forknight.Models
+{
+    public class Comment
+    {
+        public int UserID { get; set; }
+        public string Content { get; set; }
+
+        public DateTime TimePosted { get; set; }
+        public int Rating { get; set; }
+    }
+}
