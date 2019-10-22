6ca3f3c3c57f24aba27193fbf7c07b2f1fecb504
Scott-GH <thatguywhowas@live.com>
2019-10-21 20:14:30 -0700

    Added listing model

---
+++ Forknight/Models/Listing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forknight.Models
+{
+    public class Listing
+    {
+        public int Id { get; set; }
+        decimal Price { get; set; }
+
+    }
+}
